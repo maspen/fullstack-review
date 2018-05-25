f0a23511f5b692a1db905e3f33b2e1ac0defd128
Matt Aspen <mattaspen@gmail.com>
2018-05-25 14:56:45 -0700

    got mongoose get to work ... still not sure how to handle promises

--- database/index.js
+++ database/index.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher', {useMongoClient: true});
+
+const cannedDate = require('../data.json');
 
 let repoSchema = mongoose.Schema({
   id: Number,
@@ -7,15 +9,70 @@ let repoSchema = mongoose.Schema({
   ownerLogin: String,
   ownerUrl: String,
   description: String,
-  createdAt: Date
+  createdAt: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
+let save = (dataArray, callback) => {
   // This function should save a repo or repos to
   // the MongoDB
+  dataArray.forEach(function(entry, index) {
+  	var data = { id: entry.id,
+  		name: entry.name,
+  		ownerLogin: entry.owner.login,
+  		ownerUrl: entry.owner.url,
+  		description: entry.description,
+  		createdAt: entry.created_at
+  	};
+  	console.log(JSON.stringify('data going into Repo', data));
+  	var repo = new Repo(data);
+  	repo.save(callback);
+  });
 }
 
+let cb = function(err, repo) {
+	if(err) {
+		console.log('err saving repo:', err);
+		return;
+	}
+	console.log('repo saved successfully:', repo);
+	return;
+}
+
+// let handleSaveError = function(err) {
+// 	if (err) {
+// 		console.log('got error saving to mongo');
+// 	}
+// }
+
+//save(cannedDate, cb);
+// save(cannedDate, cb);
+
+let get = (callback) => {
+	// query.find({ name: 'Los Pollos Hermanos' }).find(callback)
+	// Repo.find().limit(25).sort({ age: -1 }).exec(callback).
+	Repo.find({}, null, { limit: 25 }, function (err, docs) {
+		if(err) {
+			console.log('got error retrieving repos:', err);
+			callback(err, null);
+			return;
+		}
+		console.log('got repos');
+		callback(null, docs)
+	});
+}
+
+let cbGet = function(err, repo) {
+	if(err) {
+		console.log('err getting repos:', err);
+		return;
+	}
+	console.log('repo retrieved:', repo);
+	return;
+}
+
+// get(cbGet);
+
 module.exports.save = save;
+module.exports.get = get;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 let app = express();
 const github = require('../helpers/github.js');
+const mongoose = require('../database/index.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
 
@@ -11,19 +12,31 @@ app.post('/repos', function (req, res) {
   // getFromGithub
   github.getReposByUsername(username, function(err, res){
   	if (err) {
-  		console.log('got error retrieving github data for ' + username);
-  		// TODO: send res w/ error code
+  		console.log('error retrieving github data for ' + username);
+  		res.write('error retrieving github data: ', error);
+			res.status(500).send();
   		return;
   	}
   	console.log('got github repo data from github for ' + username);
-  	// TODO: send res / success
-  	// TODO: save to db
+  	mongoose.save(res.body, function(err) {
+  		if (err) {
+  			console.log('error saving repo for ' + username + ' :', err)
+  			res.write('error: ', error);
+				res.status(500).send();
+  			return;
+  		}
+  		console.log('github repos saved for user ', username);
+  		res.write('success');
+			res.status(200).send();
+  	})
   })
 });
 
 app.get('/repos', function (req, res) {
-  // TODO - your code here!
   // This route should send back the top 25 repos
+  github.get(function(err, docs){
+  	
+  });
 });
 
 let port = 1128;
